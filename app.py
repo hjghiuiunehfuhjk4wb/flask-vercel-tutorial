856bc13791369ec54415057f2f9e8c2bf5e45c77
hjghiuiunehfuhjk4wb <214203@clvsc.tyc.edu.tw>
2025-02-13 14:54:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,28 +15,3 @@ line_handler = WebhookHandler(LINE_CHANNEL_SECRET)
 @app.route('/')
 def home():
     return "LINE BOT 首頁"
-
-@app.route("/callback", methods=["POST"])
-def callback():
-    signature = request.headers["X-Line-Signature"]
-    body = request.get_data(as_text=True)
-
-    try:
-        line_handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    return "OK"
-
-@line_handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    user_message = event.message.text
-    reply_message = f"你說了：{user_message}"
-    
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=reply_message)
-    )
-
-if __name__ == "__main__":
-    app.run(port=8000)
